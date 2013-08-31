e77176bc4fb68ee2151e56ed3cb43109d57d04af
Scot Hacker <shacker@birdhouse.org>
2013-08-31 09:55:19 -0700

    Named URL tweaks for Django 1.5 compatibility

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.conf.urls import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
