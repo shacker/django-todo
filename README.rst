dadb8f1ff65ba1e609e2cb11b56c0b4cbd03ead4
Peter Bittner <django@bittner.it>
2016-04-10 19:42:24 +0200

    Add tox configuration and integration with setup.py
    Add Travis CI build server configuration
    
    Add badges and Tests section to README
    
    Bump version (development)

--- README.rst
+++ README.rst
@@ -1,9 +1,11 @@
-===========
-django todo
-===========
+============================
+django todo |latest-version|
+============================
+
+|build-status| |health| |downloads| |license|
 
 django-todo is a pluggable multi-user, multi-group task management and
-assignment application for Django. It can serve as anything from a  personal
+assignment application for Django. It can serve as anything from a personal
 to-do system to a complete, working ticketing system for organizations.
 
 Documentation
@@ -19,3 +21,32 @@ For documentation, see the django-todo wiki pages:
 
 - `Version history
   <http://github.com/shacker/django-todo/wiki/Version-history>`_
+
+Tests
+=====
+
+Serious tests are missing, but we're checking PEP8 conformity of our syntax on
+both Python 2 and 3 using ``tox``.  You can run the tests locally via::
+
+    $ python setup.py test
+
+No prerequisites are required, all test dependencies will be installed
+automatically by ``tox`` in virtual environments created on the fly.
+Unfortunately, you'll have to install ``virtualenv`` for this to work, though.
+
+
+.. |latest-version| image:: https://img.shields.io/pypi/v/django-todo.svg
+   :alt: Latest version on PyPI
+   :target: https://pypi.python.org/pypi/django-todo
+.. |build-status| image:: https://travis-ci.org/shacker/django-todo.svg
+   :alt: Build status
+   :target: https://travis-ci.org/shacker/django-todo
+.. |health| image:: https://landscape.io/github/shacker/django-todo/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/shacker/django-todo/master
+   :alt: Code health
+.. |downloads| image:: https://img.shields.io/pypi/dm/django-todo.svg
+   :alt: Monthly downloads from PyPI
+   :target: https://pypi.python.org/pypi/django-todo
+.. |license| image:: https://img.shields.io/pypi/l/django-todo.svg
+   :alt: Software license
+   :target: https://github.com/shacker/django-todo/blob/master/LICENSE
--- setup.py
+++ setup.py
@@ -1,26 +1,59 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import todo
+from setuptools.command.test import test as TestCommand
+import shlex
+import sys
+
+import todo as package
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
 
 setup(
     name='django-todo',
-    version=todo.__version__,
-    description='A multi-user, multi-group task management and assignment system for Django.',
-    author=todo.__author__,
-    author_email=todo.__email__,
-    url=todo.__url__,
-    license=todo.__license__,
+    version=package.__version__,
+    description=package.__doc__.strip(),
+    author=package.__author__,
+    author_email=package.__email__,
+    url=package.__url__,
+    license=package.__license__,
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Framework :: Django',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Groupware',
+        'Topic :: Software Development :: Bug Tracking',
     ],
     include_package_data=True,
     zip_safe=False,
+    tests_require=['tox'],
+    cmdclass={
+        'test': Tox,
+    },
 )
