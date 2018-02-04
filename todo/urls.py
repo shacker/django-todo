58ac51a260c7e02fb6d05a242aa9a64ed9466928
Scot Hacker <shacker@birdhouse.org>
2018-02-03 16:24:52 -0800

    URLs tweak

--- todo/urls.py
+++ todo/urls.py
@@ -7,10 +7,10 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.list_lists, name="lists"),
     path('mine/', views.view_list, {'list_slug': 'mine'}, name="mine"),
-    path('<int:list_id>/<str:list_slug>/delete$', views.del_list, name="del_list"),
+    path('<int:list_id>/<str:list_slug>/delete/', views.del_list, name="del_list"),
     path('task/<int:task_id>', views.view_task, name='task_detail'),
     path('<int:list_id>/<str:list_slug>', views.view_list, name='incomplete_tasks'),
-    path('<int:list_id>/<str:list_slug>/completed$', views.view_list, {'view_completed': True}, name='completed_tasks'),
+    path('<int:list_id>/<str:list_slug>/completed/', views.view_list, {'view_completed': True}, name='completed_tasks'),
     path('add_list/', views.add_list, name="add_list"),
 
     # FIXME need both of these?
