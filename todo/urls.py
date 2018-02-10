0c8d31b6af33ca1a3994a1a2979d196f69234f6e
Scot Hacker <shacker@birdhouse.org>
2018-02-10 00:26:19 -0800

    Clean up URLs

--- todo/urls.py
+++ todo/urls.py
@@ -8,18 +8,48 @@ urlpatterns = [
     path('', views.list_lists, name="lists"),
 
     # Three paths into `list_detail` view
-    path('<int:list_id>/<str:list_slug>/', views.list_detail, name='list_detail'),
-    path('mine/', views.list_detail, {'list_slug': 'mine'}, name="mine"),
-    path('<int:list_id>/<str:list_slug>/completed/', views.list_detail, {'view_completed': True}, name='completed_tasks'),
+    path(
+        'mine/',
+        views.list_detail,
+        {'list_slug': 'mine'},
+        name="mine"),
 
-    path('<int:list_id>/<str:list_slug>/delete/', views.del_list, name="del_list"),
-    path('add_list/', views.add_list, name="add_list"),
+    path(
+        '<int:list_id>/<str:list_slug>/',
+        views.list_detail,
+        name='list_detail'),
 
-    path('task/<int:task_id>/', views.task_detail, name='task_detail'),
+    path(
+        '<int:list_id>/<str:list_slug>/completed/',
+        views.list_detail,
+        {'view_completed': True},
+        name='list_detail_completed'),
+
+    path(
+        '<int:list_id>/<str:list_slug>/delete/',
+        views.del_list,
+        name="del_list"),
+
+    path(
+        'add_list/',
+        views.add_list,
+        name="add_list"),
+
+    path(
+        'task/<int:task_id>/',
+        views.task_detail,
+        name='task_detail'),
 
     # FIXME need both of these?
-    path('search-post/', views.search_post, name="search-post"),
-    path('search/', views.search, name="search"),
+    path(
+        'search-post/',
+        views.search_post,
+        name="search-post"),
+
+    path(
+        'search/',
+        views.search,
+        name="search"),
 
     # View reorder_tasks is only called by JQuery for drag/drop task ordering
     path('reorder_tasks/', views.reorder_tasks, name="reorder_tasks"),
