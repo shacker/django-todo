598cf7d79ba5efa01bdf7eeae909dda03b2b9ca0
Adrien Lemaire <lemaire.adrien@gmail.com>
2019-04-24 12:55:22 +0900

    chore: ignore db file generated by make_migration

--- test_settings.py
+++ test_settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "test"
+        "NAME": ".testdb"
     }
 }
 
